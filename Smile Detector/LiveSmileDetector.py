9df143370fe5edfc0b4f0e2bf14748c9fa7faa85
Pedram Ghazi <pedram.ghazi@tut.fi>
2017-11-21 15:10:47 +0200

    Syncronising smile detection for multiple faces implemented with minor bug!

---
+++ Smile Detector/LiveSmileDetector.py
@@ -0,0 +1,66 @@
+import cv2
+import RecognitionThread
+import copy
+import numpy as np
+
+
+class LiveSmileDetector:
+    prediction_label = 'Not smile'
+
+    def __init__(self):
+        self.cap = cv2.VideoCapture(2)
+        self.ret, tmp_img = self.cap.read()
+        self.face_in_frame = [0, 0, 224, 224, tmp_img]
+        self.prediction_label = 'Not smile'
+        self.x = 0
+        self.y = 0
+        self.w = 0
+        self.h = 0
+
+    def get_face(self):
+        return self.face_in_frame
+
+    def set_smile(self, prediction, x, y, w, h):
+        self.prediction_label = 'Smile' if prediction.item(1) >= prediction.item(0) else 'Not smile'
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+
+
+lsd = LiveSmileDetector()
+RT_instance = RecognitionThread.RT(lsd)
+
+while True:
+    # Capture frame-by-frame
+    ret, frame = lsd.cap.read()
+
+    # Changing frame's color order
+    colored_frame = cv2.cvtColor(frame, cv2.COLOR_BGRA2BGR)
+
+    # Detecting faces in the frame
+    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    faces = face_cascade.detectMultiScale(frame, 1.3, 5)
+
+    # Drawing face rectangles around detected faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(colored_frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        # Cropping the face out of the frame
+        detected_face = colored_frame[y:y + h, x:x + w, ...]
+
+        # Updating the frame and face coordinates in it
+        lsd.face_in_frame = [x, y, w, h, colored_frame]
+
+        # Display the resulting prediction
+        cv2.putText(colored_frame, lsd.prediction_label, (lsd.x + lsd.w // 4, lsd.y + lsd.h - 10),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), thickness=2)
+
+    # Display images from the camera
+    cv2.imshow('cropped_frame', colored_frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+lsd.cap.release()
+cv2.destroyAllWindows()
--- Smile Detector/RecognitionThread.py
+++ Smile Detector/RecognitionThread.py
@@ -10,25 +10,56 @@ import tensorflow as tf
 class RT(threading.Thread):
     def __init__(self, lsd):
         threading.Thread.__init__(self)
-        self.lsd = lsd
+        self.graph = tf.get_default_graph()
         self.daemon = True
+        #  Loading model
         self.model_name = 'mobilenet_0.75_224_0_model.h5'
         self.model = load_model(self.model_name, custom_objects={'relu6': relu6, 'DepthwiseConv2D': DepthwiseConv2D})
-        self.graph = tf.get_default_graph()
+        # Initialising the variables
+        self.lsd = lsd
+        self.w = 0
+        self.h = 0
+        # Starting the thread
         self.start()
 
     def run(self):
+        # Our operations on the frame come here
         while True:
-            face = self.lsd.getFace()
+            # Getting the new frame and coordinates of face in it
+            face_in_frame = self.lsd.get_face()
+            # Changing frame color order
+            frame = cv2.cvtColor(face_in_frame[4], cv2.COLOR_BGR2RGB)
+            # Adding zero padding around the image
+            frame_size = np.shape(frame)
+            frame = cv2.copyMakeBorder(frame, int(frame_size[0] / 2), int(frame_size[0] / 2), int(frame_size[1] / 2),
+                                       int(frame_size[1] / 2), cv2.BORDER_CONSTANT)
 
-            # Smile detecting
-            face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
-            if np.shape(face) != (224,224,3):
-                face = face[:224, :224, ...]
-            img_array = image.img_to_array(face)
-            img_array = np.expand_dims(img_array, axis=0)
-            img_array /= 255.
+            # Updating coordinates according to the added zero padding
+            x = face_in_frame[0] + int(frame_size[1] / 2)
+            y = face_in_frame[1] + int(frame_size[0] / 2)
+            self.w = face_in_frame[2]
+            self.h = face_in_frame[3]
 
-            with self.graph.as_default():
-                prediction = self.model.predict(img_array)
-                self.lsd.setSmile(prediction)
+            # Preprocessing image of the detected face to have size 224x224
+            face_rec_len = min(self.w, self.h)
+            if face_rec_len <= 224:
+                tmp_len = (224 - face_rec_len) // 2
+                detected_face = frame[
+                                y - tmp_len:y + self.h + tmp_len + (face_rec_len % 2),
+                                x - tmp_len:x + self.w + tmp_len + (face_rec_len % 2), ...]
+                self.smile_detector(detected_face, x - int(frame_size[1] / 2), y - int(frame_size[0] / 2))
+            elif face_rec_len > 224:
+                detected_face = cv2.resize(frame[y - int(face_rec_len/2):y + int(1.5 * face_rec_len),
+                                           x - int(face_rec_len):x + int(1.5 * face_rec_len), ...], (224, 224))
+                self.smile_detector(detected_face, x - int(frame_size[1] / 2), y - int(frame_size[0] / 2))
+
+    def smile_detector(self, detected_face, x, y):
+        # Preprocess image od detected face
+        img_array = image.img_to_array(detected_face)
+        img_array = np.expand_dims(img_array, axis=0)
+        img_array /= 255.
+
+        # Smile detecting and passing the result to the main thread
+        with self.graph.as_default():
+            prediction = self.model.predict(img_array)
+            self.lsd.set_smile(prediction, x, y, self.w, self.h)
