6cbb37396b387684dd4161d1f62c63f10ac8ff16
Pedram ghazi <pedram.ghz@gmail.com>
2017-11-27 16:18:19 +0200

    issues solved finaly

--- Smile Detector/RecognitionThread.py
+++ Smile Detector/RecognitionThread.py
@@ -24,49 +24,40 @@ class RT(threading.Thread):
         # Retrieving new frames form the main thread in an infinite loop
         while True:
             # Getting the new frame and coordinates of face in it
-            faces_and_frame = self.lsd.get_face()
+            face_list_and_frame = self.lsd.get_face()
             # Getting face list
-            faces = faces_and_frame[0]
+            faces = face_list_and_frame[0]
             # Getting frame and changing its color order
-            frame = cv2.cvtColor(faces_and_frame[1], cv2.COLOR_BGR2RGB)
+            frame = cv2.cvtColor(face_list_and_frame[1], cv2.COLOR_BGR2RGB)
             # Adding zero padding around the image
             frame_size = np.shape(frame)
-            frame = cv2.copyMakeBorder(frame, int(frame_size[0]), int(frame_size[0]), int(frame_size[1]),
-                                       int(frame_size[1]), cv2.BORDER_CONSTANT)
-            faces_prediction_results = []
+            frame = cv2.copyMakeBorder(frame, int(frame_size[0] / 2), int(frame_size[0] / 2), int(frame_size[1] / 2),
+                                       int(frame_size[1] / 2), cv2.BORDER_CONSTANT)
+            result = []
             for (x, y, w, h) in faces:
                 # Updating coordinates of top-left corner of face according to the added zero padding
                 padded_x = x + int(frame_size[1] / 2)
                 padded_y = y + int(frame_size[0] / 2)
 
-                # Preprocessing image of the detected face by adding padding to it and resizing it
+                # Preprocessing image of the detected face to have size 224x224
                 face_rec_len = min(w, h)
-                if face_rec_len <= 185:
-                    print(0)
-                    img_padding = (224 - face_rec_len) // 2
+                if face_rec_len <= 224:
+                    tmp_len = (224 - face_rec_len) // 2
                     detected_face = frame[
-                                    padded_y - img_padding:padded_y + h + img_padding + (face_rec_len % 2),
-                                    padded_x - img_padding:padded_x + w + img_padding + (face_rec_len % 2), ...]
-                    prediction_result = self.smile_detector(detected_face)
-                elif face_rec_len < 224:
-                    print(1)
-                    img_padding = 40
-                    detected_face = frame[padded_y - img_padding:padded_y + h + img_padding,
-                                    padded_x - img_padding:padded_x + w + img_padding, ...]
-                    detected_face = cv2.resize(detected_face, (224, 224))
-                    prediction_result = self.smile_detector(detected_face)
-                else:
-                    print(2)
+                                    padded_y - tmp_len:padded_y + h + tmp_len + (face_rec_len % 2),
+                                    padded_x - tmp_len:padded_x + w + tmp_len + (face_rec_len % 2), ...]
+                    res = self.smile_detector(detected_face)
+                elif face_rec_len > 224:
                     detected_face = cv2.resize(
                         frame[padded_y - int(face_rec_len / 2):padded_y + int(1.5 * face_rec_len),
                         padded_x - int(face_rec_len):padded_x + int(1.5 * face_rec_len), ...], (224, 224))
-                    prediction_result = self.smile_detector(detected_face)
+                    res = self.smile_detector(detected_face)
 
                 # Appending result of prediction and its coordinates to the list
-                faces_prediction_results.append([prediction_result, x, y, w, h])
+                result.append([res, x, y, w, h])
 
             # Updating smile predictions with their corresponding coordinates
-            self.lsd.set_smile(faces_prediction_results)
+            self.lsd.set_smile(result)
 
     def smile_detector(self, detected_face):
         # Preprocess image od detected face
